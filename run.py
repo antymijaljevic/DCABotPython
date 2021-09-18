c5754814a4d5166c88ae86b9c6f4fbfba17cf0c0
Anty Mijaljevic <antymijaljevic@gmail.com>
2021-09-18 17:31:47 +0100

    last upload

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #written by antymijaljevic@gmail.com
 
-import ccxt, telegram_send,schedule, time
+import ccxt, telegram_send, schedule, time, multiprocessing
 from keys import API_KEY, SECRET
 
 
@@ -42,7 +42,7 @@ def buyAda():
     type = 'market'  # or 'limt'
     side = 'buy'  # or 'sell'
     amount = 1 # ignore this one
-    price = 11  # dollar investment every day
+    price = 10  # dollar investment every day
     data = exchange.create_order(symbol, type, side, amount, price)
     report = "***BUYING ORDER FULFILLED***\n\n" + "Date: " + str(data.get('datetime')[0:10]) + "\n" + "Time: " + str(data.get('datetime')[11:19]) + "\n" + "Amount: " + str(data.get('amount')) + " ADA" + "\n" + "Cost: " + str(data.get('cost')) + " $"
     #telegram notification
@@ -54,15 +54,43 @@ def buySol():
     type = 'market'  # or 'limit'
     side = 'buy'  # or 'sell'
     amount = 1 # ignore this one
-    price = 11  # dollar investment every day
+    price = 10  # dollar investment every day
     data = exchange.create_order(symbol, type, side, amount, price)
     report = "***BUYING ORDER FULFILLED***\n\n" + "Date: " + str(data.get('datetime')[0:10]) + "\n" + "Time: " + str(data.get('datetime')[11:19]) + "\n" + "Amount: " + str(data.get('amount')) + " SOL" + "\n" + "Cost: " + str(data.get('cost')) + " $"
     #telegram notification
     telegram_send.send(messages=[report])
 
 
+if __name__ == '__main__':
+    # multiprocessing 1 | spot wallet and ada/sol prices 2| orders
+    first_process = multiprocessing.Process(name='first_process', target=buyAda)
+    second_process = multiprocessing.Process(name='second_process', target=buySol)
 
-percentageReport()
+    #set specific time to repeat the processes
+    # schedule.every(70).seconds.do(first_process.run)
+    schedule.every().day.at("04:00").do(first_process.run)
+    schedule.every().day.at("04:30").do(second_process.run)
+
+    schedule.run_pending()
+
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # try:
 # 	percentageReport()
 # except:
