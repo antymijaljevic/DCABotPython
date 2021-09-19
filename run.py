5387f68f5285eb87ec587210d8a9593eabecc195
Anty Mijaljevic <antymijaljevic@gmail.com>
2021-09-19 10:11:35 +0100

    fixing bugs

--- run.py
+++ run.py
@@ -11,99 +11,110 @@ exchange = ccxt.binance({
     'enableRateLimit': True,
 })
 
+#trading set up
+orderOnePair = 'ADA/BUSD'
+orderTwoPair = 'SOL/BUSD'
+#investment
+orderOneInvestment = 10
+orderTwoInvestment = 10
+#time of buying every day
+orderOne = "01:15"
+orderTwo = "01:15"
+#timestamp
+now = str(datetime.now())
+
+
 #spot wallet balance
 def walletBalance():
 	balance = exchange.fetch_total_balance()
-	balancePrint = "***SPOT WALLET BALANCE***\n\n" + "BUSD = " + str(int(balance['BUSD'])) + "$" + "\n" + "ADA = " + str(int(balance['ADA'])) + " ADA" + "\n" "SOL = " + str(int(balance['SOL'])) + " S"
+	balancePrint = "***SPOT WALLET BALANCE***\n\n" + "DATE: " + now[:16] + "\n" +  "BUSD = " + str(int(balance['BUSD'])) + "$" + "\n" + orderOnePair[:3] + " = " + str(int(balance[orderOnePair[:3]])) + " " + orderOnePair[:1]  + "\n" + orderTwoPair[:3] + " = " + str(int(balance[orderTwoPair[:3]])) + " " + orderTwoPair[0:1]
 	#telegram notification
 	telegram_send.send(messages=[balancePrint])
 
 
-#ada and sol last prices telegram notification
+#prices of the pairs telegram notification
 def lastPrices():
-	currentAdaPrice = exchange.fetch_ticker('ADA/BUSD')
-	currentSolPrice = exchange.fetch_ticker('SOL/BUSD')
-	priceReport = "***CURRENT PRICES***\n\n" + "ADA >> " + str(round(currentAdaPrice['last'], 2))+"$" + "\n" + "SOL >> " + str(round(currentSolPrice['last'], 2))+"$"
+	pairOnePrice = exchange.fetch_ticker(orderOnePair)
+	pairTwoPrice = exchange.fetch_ticker(orderTwoPair)
+	priceReport = "***CURRENT PRICES***\n\n" + "DATE: " + now[:16] + "\n" + orderOnePair[:3] + " >> " + str(round(pairOnePrice['last'], 2))+"$" + "\n" + orderTwoPair[:3] + " >> " + str(round(pairTwoPrice['last'], 2))+"$"
 	telegram_send.send(messages=[priceReport])
 
 #for this method please check google spreadsheet which I shared with you, sheet"Every day 10$ + 60$ on -10% dips"
 def percentageReport():
-    ada = exchange.fetch_ticker('ADA/BUSD')
-    ada = round(float(ada['info']['priceChangePercent']), 2)
-    sol = exchange.fetch_ticker('SOL/BUSD')
-    sol = round(float(sol['info']['priceChangePercent']), 2)
+    firstPair = exchange.fetch_ticker(orderOnePair)
+    firstPairP = round(float(firstPair['info']['priceChangePercent']), 2)
+    secondPair = exchange.fetch_ticker(orderTwoPair)
+    secondPairP = round(float(secondPair['info']['priceChangePercent']), 2)
     #alert message
-    alert = "***DIP ALERT!***\n\n" + "ADA >> " + str(ada) + " %" + "\nSOL >> " + str(sol) + " %\n\nDo you want to invest more?"
+    alert = "***DIP ALERT!***\n\n" + "DATE: " + now[:16] + "\n" + orderOnePair[0:3] + " >> " + str(firstPairP) + " %\n" + orderTwoPair[0:3] + " >> " + str(secondPairP) + " %\n\nDo you want to invest more?"
 
     #if there is dip more than -10% it notifies you to invest extra money manually
-    if ada < -9.99 or sol < -9.99:
-        print(ada, sol)
+    if firstPairP < 1 or secondPairP < -9.99:
         telegram_send.send(messages=[alert])
         #possible call on buyAda function with 60$ extra or let user to buy manually
     else:
-        now = str(datetime.now())
         print("SCRIPT STATUS ... OK", now[:16])
 
-#buy or sell ada
+#first pair trade
 #I have in plan to implement pysheet library to save data directly to google spreadsheet
-def buyAda():
-    symbol = 'ADA/BUSD'  
+def order_1():
+    symbol = orderOnePair
     type = 'market'  # or 'limt'
     side = 'buy'  # or 'sell'
     amount = 1 # ignore this one
-    price = 10  # dollar investment every day
+    price = orderOneInvestment
     try:
         data = exchange.create_order(symbol, type, side, amount, price)
         #send report
-        report = "***BUYING ORDER FULFILLED***\n\n" + "Date: " + str(data.get('datetime')[0:1]) + "\n" + "Time: " + str(data.get('datetime')[11:19]) + "\n" + "Amount: " + str(data.get('amount')) + " ADA" + "\n" + "Cost: " + str(data.get('cost')) + " $"
+        report = "***BUYING ORDER FULFILLED***\n\n" + "Date: " + now[:16] + "\n" + "Amount: " + str(data.get('amount')) + orderOnePair[:3] + "\n" + "Cost: " + str(data.get('cost')) + " $"
         telegram_send.send(messages=[report])
     except:
         telegram_send.send(messages=["Binance API connection issue!\nADA buying order failed"])
 
-#buy or sell sol
-def buySol():
-    symbol = 'SOL/BUSD'  
+#second pair trade
+def order_2():
+    symbol = orderTwoPair  
     type = 'market'  # or 'limit'
     side = 'buy'  # or 'sell'
     amount = 1 # ignore this one
-    price = 10  # dollar investment every day
+    price = orderTwoInvestment
     try:
         data = exchange.create_order(symbol, type, side, amount, price)
         #send report    
-        report = "***BUYING ORDER FULFILLED***\n\n" + "Date: " + str(data.get('datetime')[0:10]) + "\n" + "Time: " + str(data.get('datetime')[11:19]) + "\n" + "Amount: " + str(data.get('amount')) + " SOL" + "\n" + "Cost: " + str(data.get('cost')) + " $"
+        report = "***BUYING ORDER FULFILLED***\n\n" + "Date: " + now[:16] + "\n" + "Amount: " + str(data.get('amount')) + orderTwoPair[:3] + "\n" + "Cost: " + str(data.get('cost')) + " $"
         telegram_send.send(messages=[report])
     except:
         telegram_send.send(messages=["Binance API connection issue!\nSOL buying order failed"])
 
 if __name__ == '__main__':
-    # multiprocessing 1|spot wallet 2| last price ada/sol 3| dip alert 4| buy ada 5| buy sol
+    # multiprocessing 1|spot wallet 2| last price ada/sol 3| dip alert 4| first pair trade 5| second pair trade
     first_process = multiprocessing.Process(name='first_process', target=walletBalance)
     second_process = multiprocessing.Process(name='second_process', target=lastPrices)
     third_process = multiprocessing.Process(name='third_process', target=percentageReport)
-    fourth_process = multiprocessing.Process(name='fourth_process', target=buyAda)
-    fifth_process = multiprocessing.Process(name='fifth_process', target=buySol)
+    fourth_process = multiprocessing.Process(name='fourth_process', target=order_1)
+    fifth_process = multiprocessing.Process(name='fifth_process', target=orderTwoPair)
 
     #running processes/functions in different times
     #https://schedule.readthedocs.io/en/stable/examples.html
 
-    # 1| Spot wallet report
-    schedule.every().day.at("12:00").do(first_process.run)
-    # 2| last price ada/sol
-    schedule.every(2).hours.do(second_process.run)
-    # 3| dip alert
-    schedule.every(4).hours.do(third_process.run)
-    # 4| buy ada
-    schedule.every().day.at("03:30").do(fourth_process.run)
-    # 5| buy sol
-    schedule.every(48).hours.do(fifth_process.run)
+    # # 1| Spot wallet report
+    # schedule.every().day.at("12:00").do(first_process.run)
+    # # 2| last price ada/sol
+    # schedule.every(2).hours.do(second_process.run)
+    # # 3| dip alert
+    # schedule.every(4).hours.do(third_process.run)
+    # # 4| first pair trade
+    # schedule.every().day.at(orderOne).do(fourth_process.run)
+    # # 5| second pair trade
+    # schedule.every().day.at(orderTwo).do(fifth_process.run)
 
     #only for testing purposes
-    # #spot wallet report
+    #spot wallet report
     # schedule.every(3).seconds.do(first_process.run)
     # #last price report
     # schedule.every(3).seconds.do(second_process.run)
     # #dip alert
-    # schedule.every(3).seconds.do(third_process.run)
+    schedule.every(3).seconds.do(third_process.run)
     # #buy ada
     # schedule.every(3).seconds.do(fourth_process.run)
     # #buy sol
