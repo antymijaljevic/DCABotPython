e11c38a7ba2184ad729b1fa5c31ee9ee5d9bd4c9
Anty Mijaljevic <antymijaljevic@gmail.com>
2021-09-22 08:10:26 +0100

    Fully stable version

--- run.py
+++ run.py
@@ -12,8 +12,8 @@ exchange = ccxt.binance({
     'secret': SECRET,
     'enableRateLimit': True,
 })
-# set TRUE for the binance testnet, and set test pairs
-exchange.set_sandbox_mode(False)
+# set TRUE for the binance testnet, set test pairs and change to API test key
+# exchange.set_sandbox_mode(True)
 # orderPair_1 = 'BTC/BUSD'
 # orderPair_2 = 'ETH/BUSD'
 
@@ -22,7 +22,7 @@ exchange.set_sandbox_mode(False)
 # trading variables for order function
 orderPair_1 = 'ADA/BUSD' # bug 1 regEx
 orderPair_2 = 'ETH/BUSD'
-orderTime = '01:15'
+orderTime = '01:00'
 orderInvestment = 10
 
 
@@ -34,7 +34,7 @@ def sendSheetReport(sheetNum, reportNum, p_1, p_2, p_3, p_4=None, p_5=None, p_6=
     sheetReports =[
         [p_1[:19], p_2, str(p_3)+"$"], # spot wallet
         [p_1[:19], p_2, str(p_3)+"%", p_4], # dip alert
-        [p_1[:19], p_2, p_3, p_4, p_5, p_6] # buying order
+        [p_1[:19], p_2, p_3, p_4, p_5, p_6] # buying order pair one and two
     ]
     sheet.append_row(sheetReports[reportNum])
 
@@ -67,7 +67,7 @@ def dipAlert():
     pairsPer = {orderPair_1[:3]:[round(float(pairOneInfo['info']['priceChangePercent']), 2), pairOnePrice], orderPair_2[:3]:[round(float(pairTwoInfo['info']['priceChangePercent']), 2), pairTwoPrice]}
 
     for key, value in pairsPer.items():
-        if value[0] < -10:
+        if value[0] < 3:
             ticker = key
             percentage = value[0]
             price = value[1]
@@ -75,7 +75,9 @@ def dipAlert():
             sendSheetReport(2, 1, now, ticker, percentage, price)
             telMsg = "***DIP ALERT!***\n\nDate: "+now[:19]+"\n"+ticker+" >>  "+str(percentage)+"%"+" at price "+ str(price)+"$"
             telegram_send.send(messages=[telMsg])
-            print(key+" DIP ALERT ... SENT", now[:19])
+            print(key+" DIP ALERT ... BUYING REQUEST HAS BEEN SENT", now[:19])
+            order(ticker+"/BUSD", 60)
+            print(ticker + " DIP BUYING ORDER HAS BEEN EXECUTED", now[:19])
         else:
             print(key + ", NO EXTREME DIPS. SCRIPT ... RUNNING", now[:19])
 
@@ -94,6 +96,20 @@ def order(thePair=None, theInvestment=None):
     amount = 1 # ignore this one
     price = theInvestment
     # request order
+    #for testing only
+    # dataBinance = {
+    #     'info': {'symbol': 'ETHBUSD', 'orderId': '574926833', 'orderListId': '-1', 'clientOrderId': 'x-R4BD3S821058420866ddf407f78d90', 'transactTime': '1632210699815', 'price': '0.00000000', 'origQty': '4.70000000', 'executedQty': '4.70000000', 'cummulativeQuoteQty': '69.99220000', 'status': 'FILLED', 'timeInForce': 'GTC', 'type': 'MARKET', 'side': 'BUY', 'fills': [{'price': '2.12600000', 'qty': '4.70000000', 'commission': '0.00470000', 'commissionAsset': 'ETH', 'tradeId': '52730974'}]},
+    #     'timestamp': 1632210699815, 'datetime': 
+    #     '2021-09-21T07:51:39.815Z', 
+    #     'price': 2.126,  
+    #     'amount': 4.7, 
+    #     'cost': 69.2222, 
+    #     'average': 2.126, 
+    #     'filled': 4.7, 
+    #     'fee': {'cost': 0.0047, 'currency': 'ETH'}, 
+    #     'trades': [{'info': {'price': '2.12600000', 'qty': '4.70000000', 'commission': '0.00470000', 'commissionAsset': 'ETH', 'tradeId': '52730974'}, 'timestamp': None, 'datetime': None, 'symbol': 'ETH/BUSD', 'id': None, 'order': None, 'type': None, 'side': None, 'takerOrMaker': None, 'price': 2.126, 'amount': 4.7, 'cost': 69.2222, 'fee': {'cost': 0.0047, 'currency': 'ETH'}}], 'fees': [{'cost': 0.0047, 'currency': 'ETH'}]
+    # }
+
     dataBinance = exchange.create_order(symbol, type, side, amount, price)
     marketPrice = round(dataBinance['trades'][0]['price'], 2) 
     invested = round(float(dataBinance['cost']), 2)
@@ -108,7 +124,7 @@ def order(thePair=None, theInvestment=None):
     print(ticker + " ORDER FULFILLED ... DONE", now[:19])
 
 
-#schedule specific time for each function
+# #schedule specific time for each function
 #wallet balance
 schedule.every(24).hours.do(walletBalance)
 # dip alert
@@ -119,10 +135,10 @@ schedule.every().day.at(orderTime).do(order, orderPair_1, orderInvestment)
 schedule.every().day.at(orderTime).do(order, orderPair_2, orderInvestment)
 
 # # schedule specific time for each function
-# schedule.every(9).seconds.do(walletBalance)
-# schedule.every(9).seconds.do(dipAlert)
-# schedule.every(9).seconds.do(order, orderPair_1, orderInvestment)
-# schedule.every(9).seconds.do(order, orderPair_2, orderInvestment)
+# schedule.every(10).minutes.do(walletBalance)
+#schedule.every(10).seconds.do(dipAlert)
+# schedule.every(10).seconds.do(order, orderPair_1, orderInvestment)
+# schedule.every(10).seconds.do(order, orderPair_2, orderInvestment)
 
 while True:
     schedule.run_pending()
