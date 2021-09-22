ceca8c54216a6f781d16082889e72283d327be50
Anty Mijaljevic <antymijaljevic@gmail.com>
2021-09-22 21:09:30 +0100

    script updated 1

--- run.py
+++ run.py
@@ -17,37 +17,41 @@ exchange = ccxt.binance({
 # exchange.set_sandbox_mode(True)
 
 
-# variables to be set up manually by user needs
+# variables to be set up manually by a user needs
 Conversion_1 = 'ADA/BUSD' # put here you conversion pair 1
-Conversion_2 = 'ETH/USDT' # put here you conversion pair 2
+Conversion_2 = 'ETH/BUSD' # put here you conversion pair 2
 matchCon_1 = re.search(r'([A-Z]*)/([A-Z]*)', Conversion_1)
 matchCon_2 = re.search(r'([A-Z]*)/([A-Z]*)', Conversion_2)
 Conversion_1FirstTicker = matchCon_1.group(1)
 Conversion_1SecondTicker = matchCon_1.group(2)
 Conversion_2FirstTicker = matchCon_2.group(1)
 Conversion_2SecondTicker = matchCon_2.group(2)
-orderTime = '01:00' # at what time?
+orderTime = '01:00' # at what time you want to execute buying order?
 orderInvestment = 10 # how much you want to invest?
+dipInvestment = 60 # how much you want to invest during a dip?
+dipPercentage = -10 # here you set the depth of a dip
+checkForDip = 8 # dip check frequency (in hours)
+walletBalanceCheck = "12:00" # at what time you want to get wallet balance report?
 
 
 # call on the google spreadsheet API, specific sheets and appending report into the sheet
-def sendSheetReport(sheetNum, reportNum, p_1, p_2, p_3, p_4=None, p_5=None, p_6=None):
+def sendSheetReport(sheetNum, reportNum, p_1, p_2, p_3, p_4=None, p_5=None, p_6=None, p_7=None):
     spreadsheet_credentials = gspread.service_account(filename='sheet_credentials.json')
     spreadsheet_id = spreadsheet_credentials.open_by_key('1e-I7RvxlIU3SvvW4i5OD3GCVPqj5Ka5anDbyUOc93oY')
     sheet = spreadsheet_id.get_worksheet(sheetNum)
     sheetReports =[
         [p_1[:19], p_2, str(p_3)], # spot wallet report
         [p_1[:19], p_2, str(p_3)+"%", p_4], # dip alert
-        [p_1[:19], p_2, p_3, p_4, p_5, p_6] # buying order conversion pair
+        [p_1[:19], p_2, p_3, p_4, p_5, p_6, str(p_7)+"%"] # buying order conversion pair
     ]
-    #append as new row
+    # append as new row
     sheet.append_row(sheetReports[reportNum])
 
 
 # spot wallet balance
 def walletBalance():
     now = str(datetime.now())
-    # total balance (of every ticker that exist)
+    # the balance of your spot wallet (will print all tickers that exist on the binanace)
     balance = exchange.fetch_total_balance()
     balances = {
         Conversion_1FirstTicker: float(balance[Conversion_1FirstTicker]),
@@ -67,57 +71,48 @@ def walletBalance():
 # dip alert
 def dipAlert():
     now = str(datetime.now())
-    pairOneInfo = exchange.fetch_ticker(Conversion_1)
-    pairTwoInfo = exchange.fetch_ticker(Conversion_2)
-    pairOnePrice= round(pairOneInfo['last'], 2)
-    pairTwoPrice= round(pairTwoInfo['last'], 2)
-    pairsPer = {orderPair_1[:3]:[round(float(pairOneInfo['info']['priceChangePercent']), 2), pairOnePrice], orderPair_2[:3]:[round(float(pairTwoInfo['info']['priceChangePercent']), 2), pairTwoPrice]}
+    # all informations about specific conversion pair
+    conversion_1Info = exchange.fetch_ticker(Conversion_1)
+    conversion_2Info = exchange.fetch_ticker(Conversion_2)
+    conversion_1InfoPrice= conversion_1Info['last']
+    conversion_2InfoPrice= conversion_2Info['last']
+    pairsPer = {Conversion_1FirstTicker:[round(float(conversion_1Info['info']['priceChangePercent']), 2), conversion_1InfoPrice], Conversion_2FirstTicker:[round(float(conversion_2Info['info']['priceChangePercent']), 2), conversion_2InfoPrice]}
 
-    for key, value in pairsPer.items():
-        if value[0] < -10:
-            ticker = key
-            percentage = value[0]
-            price = value[1]
-            # sending report to telegram and spreadsheet
-            sendSheetReport(2, 1, now, ticker, percentage, price)
-            telMsg = "***DIP ALERT!***\n\nDate: "+now[:19]+"\n"+ticker+" >>  "+str(percentage)+"%"+" at price "+ str(price)+"$"
+    # checking if there is any below set dip percentage
+    for ticker, percentage in pairsPer.items():
+        if percentage[0] < dipPercentage:
+            if ticker in Conversion_1:
+                pricePerIn = Conversion_1SecondTicker
+            else:
+                pricePerIn = Conversion_2SecondTicker
+
+            # sending the reports to a telegram and the spreadsheet (only for the tickers below set dip percentage)
+            sendSheetReport(2, 1, now, ticker, percentage[0], percentage[1])
+            telMsg = "***DIP ALERT!***\n\nDate: "+now[:19]+"\n"+ticker+" >>  "+str(percentage[0])+"%"+" at price "+ str(percentage[1])+" ("+pricePerIn+")"
             telegram_send.send(messages=[telMsg])
-            print(key+" DIP ALERT ... BUYING REQUEST HAS BEEN SENT", now[:19])
-            order(ticker+"/BUSD", 60)
+            print(ticker+" DIP ALERT ... BUYING REQUEST HAS BEEN SENT", now[:19])
+            order(ticker+"/"+pricePerIn, dipInvestment)
             print(ticker + " DIP BUYING ORDER HAS BEEN EXECUTED", now[:19])
         else:
-            print(key + ", NO EXTREME DIPS. SCRIPT ... RUNNING", now[:19])
+            print(ticker + ", NO DIP, STATUS... OK", now[:19])
 
 
-def order(thePair=None, theInvestment=None):
+def order(symbol, theInvestment):
     now = str(datetime.now())
     #sheet change
-    if thePair != 'ADA/BUSD':
+    if symbol != Conversion_1:
         sheet = 1
     else:
         sheet = 0
     # order parameters
-    symbol = thePair
     type = 'market'  # or 'limt'
     side = 'buy'  # or 'sell'
     amount = 1 # ignore this one
     price = theInvestment
-    # request order
-    #for testing only
-    dataBinance = {
-        'info': {'symbol': 'ETHBUSD', 'orderId': '574926833', 'orderListId': '-1', 'clientOrderId': 'x-R4BD3S821058420866ddf407f78d90', 'transactTime': '1632210699815', 'price': '0.00000000', 'origQty': '4.70000000', 'executedQty': '4.70000000', 'cummulativeQuoteQty': '69.99220000', 'status': 'FILLED', 'timeInForce': 'GTC', 'type': 'MARKET', 'side': 'BUY', 'fills': [{'price': '2.12600000', 'qty': '4.70000000', 'commission': '0.00470000', 'commissionAsset': 'ETH', 'tradeId': '52730974'}]},
-        'timestamp': 1632210699815, 'datetime': 
-        '2021-09-21T07:51:39.815Z', 
-        'price': 2.126,  
-        'amount': 4.7, 
-        'cost': 69.2222, 
-        'average': 2.126, 
-        'filled': 4.7, 
-        'fee': {'cost': 0.0047, 'currency': 'ETH'}, 
-        'trades': [{'info': {'price': '2.12600000', 'qty': '4.70000000', 'commission': '0.00470000', 'commissionAsset': 'ETH', 'tradeId': '52730974'}, 'timestamp': None, 'datetime': None, 'symbol': 'ETH/BUSD', 'id': None, 'order': None, 'type': None, 'side': None, 'takerOrMaker': None, 'price': 2.126, 'amount': 4.7, 'cost': 69.2222, 'fee': {'cost': 0.0047, 'currency': 'ETH'}}], 'fees': [{'cost': 0.0047, 'currency': 'ETH'}]
-    }
-
-    #dataBinance = exchange.create_order(symbol, type, side, amount, price)
+    # request order and get info about conversion
+    currentTickerInfo = exchange.fetch_ticker(symbol)
+    dataBinance = exchange.create_order(symbol, type, side, amount, price)
+    tickerPercentage = round(float(currentTickerInfo['info']['priceChangePercent']), 2)
     marketPrice = round(dataBinance['trades'][0]['price'], 2) 
     invested = round(float(dataBinance['cost']), 2)
     commission = float(dataBinance['trades'][0]['info']['commission'])
@@ -125,27 +120,21 @@ def order(thePair=None, theInvestment=None):
     ticker = dataBinance['trades'][0]['info']['commissionAsset']
    
     # sending report to telegram and spreadsheet
-    sendSheetReport(sheet, 2, now, marketPrice, invested, commission, assetQty, ticker)
+    sendSheetReport(sheet, 2, now, marketPrice, invested, commission, assetQty, ticker, tickerPercentage)
     telMsg = "***BUYING ORDER FULFILLED***\n\n"+"Date: "+now[:19]+"\nAt market price: "+str(marketPrice)+" $\n"+"Coin quantity: "+str(assetQty)+" "+ticker+"\n"+"Invested: "+str(invested)+ " $"
     telegram_send.send(messages=[telMsg])
     print(ticker + " ORDER FULFILLED ... DONE", now[:19])
 
 
-# #schedule specific time for each function
-# #wallet balance
-# schedule.every(24).hours.do(walletBalance)
-# # dip alert
-# schedule.every(12).hours.do(dipAlert)
-# # order 1
-# schedule.every().day.at(orderTime).do(order, Conversion_1, orderInvestment)
-# # order 2
-# schedule.every().day.at(orderTime).do(order, Conversion_2, orderInvestment)
-
-# # schedule specific time for each function
-schedule.every(5).seconds.do(walletBalance)
-# schedule.every(5).seconds.do(dipAlert)
-# schedule.every(10).seconds.do(order, orderPair_1, orderInvestment)
-# schedule.every(10).seconds.do(order, orderPair_2, orderInvestment)
+#schedule specific time for each function
+#wallet balance
+schedule.every().day.at(walletBalanceCheck).do(walletBalance)
+# dip alert
+schedule.every(checkForDip).hours.do(dipAlert)
+# order 1
+schedule.every().day.at(orderTime).do(order, Conversion_1, orderInvestment)
+# order 2
+schedule.every().day.at(orderTime).do(order, Conversion_2, orderInvestment)
 
 while True:
     schedule.run_pending()
