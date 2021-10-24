432a334ae6ddf37094ab7518d9582032cc36770e
Anty Mijaljevic <antymijaljevic@gmail.com>
2021-10-24 19:25:19 +0100

    new

--- DCA-bot.py
+++ DCA-bot.py
@@ -1,10 +1,10 @@
 from binance.client import Client
 from binance.exceptions import BinanceAPIException
-from requests.exceptions import Timeout
-from requests.exceptions import ConnectionError
+from requests.exceptions import Timeout, ConnectionError
 from binance.helpers import round_step_size
-import config, telegram_send, schedule, gspread, time, json
+import config, telegram_send, schedule, gspread, json
 from datetime import datetime
+from time import time, sleep
 from os import system
 
 system('cls')
